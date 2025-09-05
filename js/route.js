67ef37e2d9dd753c1d6889e848f15dd242bc2f4c
Davi <131999650+DaviPac@users.noreply.github.com>
2025-09-05 19:50:16 -0300

    sla

--- js/route.js
+++ js/route.js
@@ -1,3 +1,7 @@
+const repoName = window.location.hostname.includes('github.io')
+    ? '/' + window.location.pathname.split('/')[1]
+    : '';
+
 const routes = {
     404: "/pages/404.html",
     "/": "/pages/home.html",
@@ -8,17 +12,16 @@ const routes = {
 export const route = (event) => {
     event = event || window.event;
     event.preventDefault();
-    window.history.pushState({}, "", event.target.getAttribute('href'));
+    const path = event.target.getAttribute('href');
+    const url = `${repoName}${path}`;
+    window.history.pushState({}, "", url);
     handleLocation();
 };
 
 export const handleLocation = async () => {
     const path = window.location.pathname;
-    const routeFile = routes[path] || routes[404];
-    const html = await fetch(routeFile).then((data) => data.text());
+    const localPath = path.replace(repoName, "") || "/";
+    const routeFile = routes[localPath] || routes[404];
+    const html = await fetch(`${repoName}${routeFile}`).then((data) => data.text());
     document.getElementById("app").innerHTML = html;
-
-    if (path === "/dashboard") {
-        console.log("Página do Dashboard carregada!");
-    }
 };
